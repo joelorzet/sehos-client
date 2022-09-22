4265b26f033720dc54c2c0cf78c9197275579a93
ruizrodrigo <ruiz.nrodrigo@gmail.com>
2022-09-22 06:46:07 -0300

    text favs null

--- src/components/Favourites/Favourites.tsx
+++ src/components/Favourites/Favourites.tsx
@@ -32,6 +32,7 @@ export const FavouritesCards = () => {
                   sx={{ color: 'primary.main', zIndex: (theme) => theme.zIndex.drawer + 1 }}
                   open={true}
                 ><CircularProgress color="inherit" size={200}/></Backdrop>}
+        {!favs?.lenght && <Typography mt={2} variant='h6' fontWeight={100}>There are no favourites products, go to products section and select your favourites!</Typography>}
         {favs?.map(fav =>  
             (<>
                 <Card
