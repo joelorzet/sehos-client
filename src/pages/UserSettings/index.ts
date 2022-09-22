667f6dc7eb06350468433e240e4056c8ecad7b6d
Rodrigo Ruiz <ruiz.nrodrigo@gmail.com>
2022-09-22 00:49:52 -0300

    Update index.ts

--- src/pages/UserSettings/index.ts
+++ src/pages/UserSettings/index.ts
@@ -4,5 +4,4 @@ export { default as Header } from './Header';
 export * from './Orders';
 export * from './Profile';
 export { default as UserSettings } from './UserSettings';
-export * from './listItems';
 export * from './utils';
